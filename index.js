a6693ff3a67f60b06d8e3628ef83833c6262507a
Lizzie Anthony <lizziewrab@me.com>
2021-11-05 14:52:24 -0500

    commit tree 9cf7f774634811fd44d9a4d9de5f5fdcc6fbe6e9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+     const newDrivers = drivers.filter(person => person.toLowerCase() === name.toLowerCase());
+     return newDrivers;
+}
+// const result = words.filter(word => word.length > 6);
+
+// function fuzzyMatch(drivers, letters) {
+//     let newLetters = drivers.filter(firstLetter => {
+//      firstLetter.indexOf(letters) === 0
+//     });
+//     return newLetters;
+// }
+
+function fuzzyMatch(drivers, string) {
+    let correctLetters = drivers.filter(driver => driver.slice(0, string.length) === string)
+    return correctLetters
+}
+
+function matchName(object, string) {
+return object.filter(driver => driver.name === string);
+}
